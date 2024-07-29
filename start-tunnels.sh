277bc8729fbbcea4df0a3551dda9208991945358
beastarman74 <44511355+beastarman74@users.noreply.github.com>
2024-07-29 09:19:07 +0100

    Update start-tunnels.sh

--- start-tunnels.sh
+++ start-tunnels.sh
@@ -1,78 +1,61 @@
 #!/bin/sh
-
-echo "Starting SSH tunnel setup..."
+echo "Starting AutoSSH tunnel creation..."
 
 setup_tunnel() {
     echo "Setting up tunnel with the following parameters:"
-    echo "Source SSH User: $1"
-    echo "Source Host: $2"
-    echo "Source SSH Port: $3"
-    echo "Source Port: $4"
-    echo "Target SSH User: $5"
-    echo "Target Host: $6"
-    echo "Target SSH Port: $7"
-    echo "Target Port: $8"
+    echo "Tunnel User: $1"
+    echo "Tunnel Host: $2"
+    echo "Tunnel Port: $3"
+    echo "Local Port: $4"
+    echo "Remote Port: $5"
+    echo "SSH Key: $SSHKEY"
 
-    [ -z "$1" ] && echo "Error: Source SSH User is missing" && return
-    [ -z "$2" ] && echo "Error: Source Host is missing" && return
-    [ -z "$5" ] && echo "Error: Target SSH User is missing" && return
-    [ -z "$6" ] && echo "Error: Target Host is missing" && return
+    [ -z "$1" ] && echo "Error: Tunnel User is missing" && return
+    [ -z "$2" ] && echo "Error: Tunnel Host is missing" && return
+    [ -z "$3" ] && echo "Error: Tunnel Port is missing" && return
+    [ -z "$4" ] && echo "Error: Local Port is missing" && return
+    [ -z "$5" ] && echo "Error: Remote Port is missing" && return
 
-    autossh -NT -M 0 -o "ServerAliveInterval=60" -o "ServerAliveCountMax=2" -o "StrictHostKeyChecking=no" \
-            -L "*:${8}:localhost:${4}" \
+    autossh -NT -M 0 -i "/root/.ssh/${SSHKEY}" -o "ServerAliveInterval=60" -o "ServerAliveCountMax=2" -o "StrictHostKeyChecking=no" -o "ExitOnForwardFailure=yes" \
+            -L "*:${4}:localhost:${5}" \
             -p "${3}" "${1}@${2}" &
-    autossh -NT -M 0 -o "ServerAliveInterval=60" -o "ServerAliveCountMax=2" -o "StrictHostKeyChecking=no" \
-            -R "*:${8}:localhost:${8}" \
-            -p "${7}" "${5}@${6}" &
-
+			
     echo "Tunnel setup complete."
 }
 
 # Loop through a predefined number of tunnels
-for i in $(seq 1 10); do
-    source_ssh_user=""
-    source_host=""
-    source_ssh_port=""
-    source_port=""
-    target_ssh_user=""
-    target_host=""
-    target_ssh_port=""
-    target_port=""
+for i in $(seq 1 15); do
+    tunnel_user=""
+    tunnel_host=""
+    tunnel_port=""
+    local_port=""
+    remote_host=""
+    remote_port=""
 
     while IFS=': ' read -r key value; do
         value=$(echo $value | xargs) # Trim leading and trailing whitespaces
 
         case $key in
-            "tunnel${i}_source_ssh_user")
-                source_ssh_user=$value
+            "tunnel${i}_tunnel_user")
+                tunnel_user=$value
                 ;;
-            "tunnel${i}_source_host")
-                source_host=$value
+            "tunnel${i}_tunnel_host")
+                tunnel_host=$value
                 ;;
-            "tunnel${i}_source_host_ssh_port")
-                source_ssh_port=$value
+            "tunnel${i}_tunnel_port")
+                tunnel_port=$value
                 ;;
-            "tunnel${i}_source_port")
-                source_port=$value
+            "tunnel${i}_local_port")
+                local_port=$value
                 ;;
-            "tunnel${i}_target_ssh_user")
-                target_ssh_user=$value
-                ;;
-            "tunnel${i}_target_host")
-                target_host=$value
-                ;;
-            "tunnel${i}_target_host_ssh_port")
-                target_ssh_port=$value
-                ;;
-            "tunnel${i}_target_port")
-                target_port=$value
+            "tunnel${i}_remote_port")
+                remote_port=$value
                 ;;
         esac
     done < /config.yaml
 
-    if [ ! -z "$source_ssh_user" ]; then
-        setup_tunnel "$source_ssh_user" "$source_host" "$source_ssh_port" "$source_port" \
-                     "$target_ssh_user" "$target_host" "$target_ssh_port" "$target_port"
+    if [ ! -z "$tunnel_user" ]; then
+        setup_tunnel "$tunnel_user" "$tunnel_host" "$tunnel_port" "$local_port" "$remote_port"
     fi
 done
 
